df8bd388e32828bd933284712dc0dc66f36048e0
jlblatt <jlblatt+github@gmail.com>
2015-06-17 21:38:41 -0400

    Converted to include files

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SVG Social Icons</title>
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="social">
+      <ul>
+
+        <li>
+          <a>
+            <div class="icon facebook">
+              <?php echo file_get_contents("img/facebook.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li>
+          <a>
+            <div class="icon twitter">
+              <?php echo file_get_contents("img/twitter.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li>
+          <a>
+            <div class="icon linkedin">
+              <?php echo file_get_contents("img/linkedin.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li>
+          <a>
+            <div class="icon youtube">
+              <?php echo file_get_contents("img/youtube.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li>
+          <a>
+            <div class="icon pinterest">
+              <?php echo file_get_contents("img/pinterest.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li>
+          <a>
+            <div class="icon instagram">
+              <?php echo file_get_contents("img/instagram.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li>
+          <a>
+            <div class="icon googleplus">
+              <?php echo file_get_contents("img/googleplus.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+
+  </body>
+</html>
