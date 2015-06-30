bc83e19410bfaf41af7a44b0a98bf27b0ca63893
jlblatt <jlblatt+github@gmail.com>
2015-06-29 20:13:16 -0400

    Set Variations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Playing around with SVG and CSS3 transitions.
 
+Credit for the logo SVGs: [Sensational Fix](http://sensationalfix.com/flat-social-icons-eps/).
+
 This project is distributed under the GNU General Public License Version 3.
--- index.php
+++ index.php
@@ -8,7 +8,179 @@
   </head>
   <body>
 
-    <div class="social">
+    <h1>[X-Spin]</h1>
+
+    <div class="social x-spin">
+      <ul>
+
+        <li class="facebook">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/facebook.svg"); ?>
+            </div>
+            <div class="circle ">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="twitter">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/twitter.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="linkedin">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/linkedin.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="youtube">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/youtube.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="pinterest">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/pinterest.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="instagram">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/instagram.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="googleplus">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/googleplus.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+
+    <h1>[Y-Spin]</h1>
+
+    <div class="social y-spin">
+      <ul>
+
+        <li class="facebook">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/facebook.svg"); ?>
+            </div>
+            <div class="circle ">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="twitter">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/twitter.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="linkedin">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/linkedin.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="youtube">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/youtube.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="pinterest">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/pinterest.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="instagram">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/instagram.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+        
+        <li class="googleplus">
+          <a>
+            <div class="icon">
+              <?php echo file_get_contents("img/googleplus.svg"); ?>
+            </div>
+            <div class="circle">
+              <?php echo file_get_contents("img/circle.svg"); ?>
+            </div>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+
+    <h1>[XY-Spin]</h1>
+
+    <div class="social xy-spin">
       <ul>
 
         <li class="facebook">
